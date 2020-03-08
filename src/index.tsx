9ba7c358d3708a86ab2b8868b3a37b05821f45f7
samgielis <sam.gielis@theoplayer.com>
2020-03-08 18:07:12 +0100

    Remove redux from index

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,8 @@
-import { createStore, Action } from 'redux';
-import { enthusiasm } from './reducers/index';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { StoreState } from './types';
 import App from './App';
 
-const store = createStore<StoreState | undefined, Action<any>, unknown, unknown>(enthusiasm, {
-    enthusiasmLevel: 1,
-    languageName: 'I\'m fluent in Math and Klingon',
-  });
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById('root') as HTMLElement
 );
