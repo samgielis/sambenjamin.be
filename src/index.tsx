6a89a55d79df3954935c3553c236955fa1d25953
samgielis <sam.gielis@theoplayer.com>
2020-03-08 16:04:44 +0100

    Fix tutorial bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { createStore, Action } from 'redux';
 import { enthusiasm } from './reducers/index';
 import Hello from './containers/Hello';
 import { Provider } from 'react-redux';
@@ -6,10 +6,10 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { StoreState } from './types';
 
-const store = createStore<StoreState>(enthusiasm, {
+const store = createStore<StoreState | undefined, Action<any>, unknown, unknown>(enthusiasm, {
     enthusiasmLevel: 1,
-    languageName: 'TypeScript',
-});
+    languageName: 'I\'m fluent in Math and Klingon',
+  });
 
 ReactDOM.render(
     <Provider store={store}>
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,11 +1,15 @@
 import { EnthusiasmAction } from '../actions';
 import { StoreState } from '../types/index';
 import { INCREMENT_ENTHUSIASM, DECREMENT_ENTHUSIASM } from '../constants/index';
+import { Reducer, Action } from 'redux';
 
-export function enthusiasm(state: StoreState, action: EnthusiasmAction): StoreState {
+export const enthusiasm : Reducer<StoreState | undefined, Action<any>> = function enthusiasm(state: StoreState | undefined, action: EnthusiasmAction): StoreState | undefined {
+  if (!state){
+    return undefined;
+  }
   switch (action.type) {
     case INCREMENT_ENTHUSIASM:
-      return { ...state, enthusiasmLevel: state.enthusiasmLevel + 1 };
+      return { ...state, enthusiasmLevel: state.enthusiasmLevel + 1};
     case DECREMENT_ENTHUSIASM:
       return { ...state, enthusiasmLevel: Math.max(1, state.enthusiasmLevel - 1) };
   }
