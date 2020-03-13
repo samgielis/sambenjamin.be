1f2002b214bd12d2cae92a086e2f1fe86b77983d
samgielis <sam.gielis@theoplayer.com>
2020-03-13 13:49:20 +0100

    Add profile image to homepage

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Story } from '../../model/Story';
-import {StoryGrid} from './children/StoryGrid';
+import { StoryGrid } from './children/StoryGrid';
 import { Author } from '../../model/Author';
 import './HomePage.css';
+import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 export type HomePageProps = {
     stories: Story[];
     author: Author;
 }
 
-
-
 export function HomePage(props: HomePageProps) {
-  
+
     return (
         <div className="homepage">
+            <Container style={{textAlign: "center"}}>
+                <p><Link to={"/"}>
+                    <img width="100px" alt="Author portrait" src="/profile.png"></img>
+                </Link></p>
+            </Container>
+            
             <h1>{props.author.name}</h1>
             <StoryGrid stories={props.stories}></StoryGrid>
         </div>
