b2288fa2d9977a2f4c2ab3d95721ffe1ebfbad94
samgielis <sam.gielis@theoplayer.com>
2020-03-17 19:19:55 +0100

    Fix wrong input

--- src/components/pages/story/children/PhotoSet.tsx
+++ src/components/pages/story/children/PhotoSet.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./PhotoSet.css"
 import { getStoryID, Story } from "../../../model/Story";
-import {PhotoContainer} from "./PhotoWrapper";
+import {PhotoWrapper} from "./PhotoWrapper";
 
 type PhotoSetProps = {
     story: Story;
@@ -19,7 +19,7 @@ export function PhotoSet(props: PhotoSetProps) {
         for (let photoInRow = 0; photoInRow < amountOfPhotosInRow; photoInRow++, iP++) {
             columns.push(
                 <Col key={iP} md={12 / amountOfPhotosInRow}>
-                    <PhotoContainer imageUrl={`./stories/${getStoryID(props.story)}/${photos[iP].fileName}`} alt={photos[iP].fileName}></PhotoContainer>
+                    <PhotoWrapper imageUrl={`./stories/${getStoryID(props.story)}/${photos[iP].fileName}`} alt={photos[iP].fileName}></PhotoWrapper>
                 </Col>
             );
         }
