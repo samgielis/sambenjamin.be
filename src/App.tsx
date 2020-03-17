ccd7c1cd9bca60584ff8f17b08a69cb8ea55baa8
samgielis <sam.gielis@theoplayer.com>
2020-03-17 18:24:08 +0100

    Add footer with credits

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import {
   Route
 } from "react-router-dom";
 import TagPage from './components/pages/TagPage';
-import {HomePage} from './components/pages/home/HomePage';
+import { HomePage } from './components/pages/home/HomePage';
 import StoryPage from './components/pages/story/StoryPage';
 import './App.css';
 import { Story, StoryIndex, getStoryID } from './components/model/Story';
 import { downloadJSON } from "./components/util/Utils";
 import { Author } from './components/model/Author';
+import { Container } from 'react-bootstrap';
 
 type AppProps = {
   author: Author;
@@ -56,20 +57,34 @@ class App extends React.Component<AppProps, AppState> {
     })
 
     return (<Router>
-      <div>
-
+      <div id="sambenjamin.be">
         <Switch>
           {storyRoutes}
           <Route path="/t">
             <TagPage />
           </Route>
           <Route path="/">
-            <HomePage stories={this.state.stories} author={this.props.author}/>
+            <HomePage stories={this.state.stories} author={this.props.author} />
           </Route>
         </Switch>
+        <WebsiteCredits author={this.props.author}></WebsiteCredits>
       </div>
     </Router>);
   }
 }
 
 export default App;
+
+type WebsiteCreditsProps = {
+  author: Author
+}
+
+function WebsiteCredits(props: WebsiteCreditsProps) {
+  return <footer>
+    <Container fluid={true}>
+      <div className="footer-spacer">
+      </div>
+      <p>© {new Date().getFullYear()} <a href='/'>{props.author.name}</a></p>
+    </Container>
+  </footer>
+}
