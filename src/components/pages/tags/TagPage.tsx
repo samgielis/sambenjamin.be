1ce0a778c26b2ea845d5c462b029794675d960f8
samgielis <sam.gielis@theoplayer.com>
2020-03-19 19:01:25 +0100

    BNJMN-25 Add back button on top of tag page

--- src/components/pages/tags/TagPage.tsx
+++ src/components/pages/tags/TagPage.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Story } from '../../model/Story';
 import { Container } from 'react-bootstrap';
 import { StoryGrid } from '../home/children/StoryGrid';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import "./TagPage.css";
 
 export type TagPageProps = {
   tag: string
@@ -18,6 +21,10 @@ class TagPage extends React.Component<TagPageProps, {}> {
     return <div className="homepage">
             <Container style={{ textAlign: "center", padding: "1rem" }}>
                 <h1>#{this.props.tag}</h1>
+                <br></br>
+                <h4> <Link className="go-home" to="/"><FaArrowLeft /> See more stories</Link></h4>
+            </Container>
+            <Container fluid={true} style={{ textAlign: "left", }}>
             </Container>
 
             <StoryGrid stories={this.props.stories}></StoryGrid>
