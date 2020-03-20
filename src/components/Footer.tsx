dffbb2a44a4244dfe123d3dcca78d75dd58a8f77
samgielis <sam.gielis@theoplayer.com>
2020-03-20 19:47:14 +0100

    Fix broken link in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import "./Footer.css"
 import React from 'react';
 import { Author } from './model/Author';
 import { Container } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 type FooterProps = {
     author: Author
@@ -12,7 +13,7 @@ export function Footer(props: FooterProps) {
         <Container fluid={true}>
             <div className="footer-spacer">
             </div>
-            <p>© {new Date().getFullYear()} <a href='/'>{props.author.name}</a>
+            <p>© {new Date().getFullYear()} <Link to='/'>{props.author.name}</Link>
                 <br />
         Leuven, Belgium</p>
         </Container>
