bfeae2fbee8ce82110508ac0a1fae9ada4579ddf
Sam Gielis <samgielis@gmail.com>
2020-09-03 12:04:35 +0200

    Add description, keywords and author metadata to story pages

--- src/components/pages/story/StoryPage.tsx
+++ src/components/pages/story/StoryPage.tsx
@@ -1,31 +1,53 @@
-import React from 'react';
+import React from "react";
 import { Story } from "../../model/Story";
 import { StoryCover } from "./children/StoryCover";
 import { Author } from "../../model/Author";
-import { PhotoSet } from './children/PhotoSet';
-import { TagSet } from './children/TagSet';
-import { StoryDescription } from './children/StoryDescription';
-import { trackPageView } from '../../util/Utils';
+import { PhotoSet } from "./children/PhotoSet";
+import { TagSet } from "./children/TagSet";
+import { StoryDescription } from "./children/StoryDescription";
+import { trackPageView } from "../../util/Utils";
+import Helmet from "react-helmet";
+import defaultKeywords from "../../../content/Keywords";
 
 export type StoryPageProps = {
   story: Story;
   author: Author;
-}
+};
 
 class StoryPage extends React.Component<StoryPageProps, {}> {
-
   componentDidMount() {
-    document.title = `${this.props.story.title} by ${this.props.author.name} - sambenjamin.be`;
     trackPageView(window.location.pathname + window.location.search);
   }
 
+  get keywords(): string[] {
+    let storyKeywords = this.props.story.tags || [];
+    return [...storyKeywords, ...defaultKeywords];
+  }
+
   render() {
-    return <div className='story-page' >
-      <StoryCover story={this.props.story} author={this.props.author}></StoryCover>
-      <StoryDescription story={this.props.story}></StoryDescription>
-      <PhotoSet story={this.props.story}></PhotoSet>
-      <TagSet tags={this.props.story.tags || []}></TagSet>
-    </div>;
+    return (
+      <div className="story-page">
+        <Helmet
+          title={`${this.props.story.title} by ${this.props.author.name} - sambenjamin.be`}
+        >
+          <meta name="keywords" content={this.keywords.join(", ")} />
+          <meta name="author" content={this.props.author.name} />
+          <meta
+            name="description"
+            content={`${this.props.story.title} by ${this.props.author.name}. ${
+              this.props.story.subTitle || ""
+            }`}
+          />
+        </Helmet>
+        <StoryCover
+          story={this.props.story}
+          author={this.props.author}
+        ></StoryCover>
+        <StoryDescription story={this.props.story}></StoryDescription>
+        <PhotoSet story={this.props.story}></PhotoSet>
+        <TagSet tags={this.props.story.tags || []}></TagSet>
+      </div>
+    );
   }
 }
 export default StoryPage;
