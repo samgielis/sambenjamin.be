cc0010fcab682a6ef4b758f03a8d0c7ca1b56144
Sam Gielis <samgielis@gmail.com>
2020-09-03 17:02:17 +0200

    Install Gatsby and related dependencies.

---
+++ gatsby-config.ts
@@ -0,0 +1,28 @@
+// All exported variables in this file will also used in gatsby-config.js.
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+export const siteMetadata = {
+  title: `sambenjamin.be`,
+  description: `Sam benjamin's photography portfolio`,
+  author: `Sam Gielis <samgielis@gmail.com>`,
+};
+
+export const plugins = [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/`,
+    },
+  },
+  `gatsby-plugin-typescript`,
+  `gatsby-plugin-react-helmet`,
+
+  // this (optional) plugin enables Progressive Web App + Offline functionality
+  // To learn more, visit: https://gatsby.dev/offline
+  // `gatsby-plugin-offline`,
+];
