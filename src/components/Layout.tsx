0c805c1af7e37f6e560f10d8a2b016212385f1bc
Sam Gielis <samgielis@gmail.com>
2020-09-03 22:12:03 +0200

    Handle Google Analytics as side effect in layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Footer } from "./Footer";
 import { AUTHOR } from "./model/Author";
 import ScrollToTop from "./ScrollToTop";
-import './Layout.css';
+import "./Layout.css";
+import ReactGA from "react-ga";
 
 const Layout: React.FC = ({ children }) => {
+  useEffect(() => {
+    ReactGA.initialize("UA-161391381-1");
+  });
   return (
     <ScrollToTop>
       {children}
